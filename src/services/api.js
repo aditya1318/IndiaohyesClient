27d8922a24bf6fb6d748537b0d16f2ed4034eeda
Aditya mohite <adityamohite2221@gmail.com>
2021-09-15 13:36:50 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const URL = 'https://indiaohyesserver.herokuapp.com/';
+export const URL = 'https://indiaohyesserver.herokuapp.com';
 
 export default axios.create({
     baseURL: `${URL}/api`,
