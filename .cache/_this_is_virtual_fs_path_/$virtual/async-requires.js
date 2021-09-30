abc18d24687ea92888da93ecf56a624aaba66770
jeet2610 <jeetshah0@icloud.com>
2021-09-30 14:59:10 +0530

    Added new package helmet and updated api.js

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
@@ -0,0 +1,16 @@
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-about-js": () => import("./../../../src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
+  "component---src-pages-checkout-js": () => import("./../../../src/pages/checkout.js" /* webpackChunkName: "component---src-pages-checkout-js" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-login-js": () => import("./../../../src/pages/login.js" /* webpackChunkName: "component---src-pages-login-js" */),
+  "component---src-pages-me-index-js": () => import("./../../../src/pages/me/index.js" /* webpackChunkName: "component---src-pages-me-index-js" */),
+  "component---src-pages-quick-js": () => import("./../../../src/pages/quick.js" /* webpackChunkName: "component---src-pages-quick-js" */),
+  "component---src-pages-services-[slug]-js": () => import("./../../../src/pages/services/[slug].js" /* webpackChunkName: "component---src-pages-services-[slug]-js" */),
+  "component---src-pages-shop-js": () => import("./../../../src/pages/shop.js" /* webpackChunkName: "component---src-pages-shop-js" */)
+}
+
