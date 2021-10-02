1e5b9debd794db274a512f5ba55ad1b972f51c83
aditya1318 <adityamohite2221@gmail.com>
2021-10-02 19:28:58 +0530

    sticky header top

--- src/pages/index.js
+++ src/pages/index.js
@@ -128,6 +128,9 @@ const IndexPage = () => {
 						// Render all sections according to the data fetched
 						categories?.length && services?.length
 							? <section className='bg-gray-100 pt-5 rounded-xl'>
+								<ul className='flex sticky top-0 justify-between bg-violet text-gray-100 capitalize shadow-lg py-4 px-6'>
+									{categories?.map((cat, i) => <li key={i}><a href={`/#section-categories-${i}`}>{cat.title}</a></li>)}
+								</ul>
 								<div className='px-5 space-y-5'>
 									{
 										categories.map((cat, i) =>
@@ -143,9 +146,7 @@ const IndexPage = () => {
 										)
 									}
 								</div>
-								<ul className='flex sticky bottom-0 justify-between bg-violet text-gray-100 capitalize shadow-lg p-6'>
-									{categories?.map((cat, i) => <li key={i}><a href={`/#section-categories-${i}`}>{cat.title}</a></li>)}
-								</ul>
+								
 							</section>
 							: <></>
 					}
