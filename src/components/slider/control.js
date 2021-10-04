6aab383f967ab3267ce5e2ad1bd684c160a5785b
aditya1318 <adityamohite2221@gmail.com>
2021-10-05 00:55:18 +0530

    arrow bug

--- src/components/slider/control.js
+++ src/components/slider/control.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Control = props => (
-    <div className='flex justify-end pr-4'>
+    <div className='flex justify-end pr-8'>
     <button
         className={`absolute flex justify-center items-center h-12 w-12 bg-gray-300 opacity-70 rounded-full top-1/2 transition-opacity ${props.align}-8  hover:opacity-100 `}
         onClick={props.onClick}
